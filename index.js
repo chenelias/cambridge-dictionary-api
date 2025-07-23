1c61a5ccf1e01b7b2fd3f7100102b66e48456bdd
chenelias <eliaschen.dev@gmail.com>
2025-07-23 15:58:29 +0800

    fix the hint text

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const http = require("http");
 const data = require("./data");
 const port = process.env.PORT || 3000;
+const host = "0.0.0.0";
+
 const server = http.createServer(data);
 
-console.log("Server running at http://localhost:3000");
-console.log("try /api/dictionary/english/hello");
-console.log("or use / to test the api with UI");
+console.log("helloworld");
 
-server.listen(port);
+console.log(`Server running at http://${host}:${port}`);
+console.log("Try /api/dictionary/en/hello");
+console.log("Or use / to test the API with UI");
+
+server.listen(port, host, () => {
+  console.log(`Server is listening on ${host}:${port}`);
+});
